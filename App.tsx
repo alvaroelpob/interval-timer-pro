0d86652a412104a4e3edb10e241c3fb98d17a42d
Álvaro <alvaropobladoresteban9@gmail.com>
2023-04-02 11:09:58 +0200

    bottomnav icons

--- App.tsx
+++ App.tsx
@@ -4,15 +4,32 @@ import Newinterval from './components/newinterval';
 import Saved from './components/saved';
 import Settings from './components/settings';
 
+import Plus from './assets/svg/plus';
+import Bookmark from './assets/svg/bookmark';
+import Gear from './assets/svg/gear';
+
 export default function App() {
   const Tab = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+
+            if (route.name === 'Crear') {
+              return <Plus gray={!focused}/>
+            } else if (route.name === 'Guardados') {
+              return <Bookmark gray={!focused} />
+            } else if (route.name === "Configuración") {
+              return <Gear gray={!focused}/>
+            }
+
+          },
+        })}>
         <Tab.Screen name="Crear" component={Newinterval} />
         <Tab.Screen name="Guardados" component={Saved} />
-        <Tab.Screen name="Configuracion" component={Settings} />
+        <Tab.Screen name="Configuración" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
   );
