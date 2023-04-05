3b123d5de7043dfaa8a0eb24d048e4a03939dfc2
newalvaro9 <alvaropobladoresteban9@gmail.com>
2023-04-06 01:04:47 +0200

    conflict scrollview w countdown height

--- components/newinterval.tsx
+++ components/newinterval.tsx
@@ -33,86 +33,90 @@ export default function NewInterval({ db }: { db: any }) {
     };
 
     return (
-        <ScrollView style={containers.main}>
+        <View style={containers.main}>
             <StatusBar />
-            <Modal
-                animationType="none"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                    setModalVisible(!modalVisible);
-                }}>
-                <View style={styles.modalContainer}>
-                    {
-                        /^\d{2}:\d{2}:\d{2}$/.test(currentVal) ? (
-                            <TimeSelector currentValue={currentVal} setter={setter} setModalVisible={setModalVisible} />
-                        ) : (
-                            <NumberSelector currentValue={currentVal} setter={setter} setModalVisible={setModalVisible} />
-                        )
-                    }
-                </View>
-
-            </Modal>
-            {renderCountdown ? (
-                <Countdown
-                    prepTime={prepTime}
-                    activeTime={activeTime}
-                    restTime={restTime}
-                    restBetweenSets={restBetweenSets}
-                    series={series}
-                    sets={sets}
-                    setRenderCountdown={setRenderCountdown}
-                />
-            ) : (
-                <>
-                    <TouchableOpacity onPress={() => handleTouched(prepTime, setPrepTime)}>
-                        <View style={styles.create}>
-                            <Text>Preparación</Text>
-                            <Text>{prepTime}</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => handleTouched(series, setSeries)}>
-                        <View style={styles.create}>
-                            <Text>Series</Text>
-                            <Text>{series}</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => handleTouched(activeTime, setActiveTime)}>
-                        <View style={styles.create}>
-                            <Text>Activo</Text>
-                            <Text>{activeTime}</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => handleTouched(restTime, setRestTime)}>
-                        <View style={styles.create}>
-                            <Text>Descanso</Text>
-                            <Text>{restTime}</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => handleTouched(sets, setSets)}>
-                        <View style={styles.create}>
-                            <Text>Sets</Text>
-                            <Text>{sets}</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => handleTouched(restBetweenSets, setRestBetweenSets)}>
-                        <View style={styles.create}>
-                            <Text>Descanso entre sets</Text>
-                            <Text>{restBetweenSets}</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    <Button
-                        title="Create Interval Timer"
-                        onPress={handleClickStart}
+            {
+                renderCountdown ? (
+                    <Countdown
+                        prepTime={prepTime}
+                        activeTime={activeTime}
+                        restTime={restTime}
+                        restBetweenSets={restBetweenSets}
+                        series={series}
+                        sets={sets}
+                        setRenderCountdown={setRenderCountdown}
                     />
-                </>
-            )}
-        </ScrollView>
+                ) : (
+
+                    <ScrollView>
+
+                        <Modal
+                            animationType="none"
+                            transparent={true}
+                            visible={modalVisible}
+                            onRequestClose={() => {
+                                setModalVisible(!modalVisible);
+                            }}>
+                            <View style={styles.modalContainer}>
+                                {
+                                    /^\d{2}:\d{2}:\d{2}$/.test(currentVal) ? (
+                                        <TimeSelector currentValue={currentVal} setter={setter} setModalVisible={setModalVisible} />
+                                    ) : (
+                                        <NumberSelector currentValue={currentVal} setter={setter} setModalVisible={setModalVisible} />
+                                    )
+                                }
+                            </View>
+                        </Modal>
+
+                        <TouchableOpacity onPress={() => handleTouched(prepTime, setPrepTime)}>
+                            <View style={styles.create}>
+                                <Text>Preparación</Text>
+                                <Text>{prepTime}</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={() => handleTouched(series, setSeries)}>
+                            <View style={styles.create}>
+                                <Text>Series</Text>
+                                <Text>{series}</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={() => handleTouched(activeTime, setActiveTime)}>
+                            <View style={styles.create}>
+                                <Text>Activo</Text>
+                                <Text>{activeTime}</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={() => handleTouched(restTime, setRestTime)}>
+                            <View style={styles.create}>
+                                <Text>Descanso</Text>
+                                <Text>{restTime}</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={() => handleTouched(sets, setSets)}>
+                            <View style={styles.create}>
+                                <Text>Sets</Text>
+                                <Text>{sets}</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={() => handleTouched(restBetweenSets, setRestBetweenSets)}>
+                            <View style={styles.create}>
+                                <Text>Descanso entre sets</Text>
+                                <Text>{restBetweenSets}</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        <Button
+                            title="Create Interval Timer"
+                            onPress={handleClickStart}
+                        />
+                    </ScrollView>
+                )
+            }
+        </View>
     );
 }
